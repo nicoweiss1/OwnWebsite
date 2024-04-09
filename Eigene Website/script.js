edcbb682ce21fc32f1027d51b2964c88abe2974d
nicoweiss1 <niciweiss@icloud.com>
2024-04-09 23:52:26 +0200

    afd
    asfdas

--- Eigene Website/script.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.navbar a');
-
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    links.forEach(l => l.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
